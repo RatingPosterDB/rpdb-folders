62c3b2006a784855b14b66fb8ea6e31d50bbe587
Alexandru Branza <branza.alex@gmail.com>
2021-08-11 20:20:04 +0300

    Add `sub` to Accepted Subtitle Types

--- files.js
+++ files.js
@@ -1,5 +1,5 @@
 const videoTypes = ['.mkv', '.avi', '.mp4', '.m4v', '.ts']
-const subTypes = ['.srt', '.smi', '.ssa', '.ass', '.vtt']
+const subTypes = ['.srt', '.smi', '.ssa', '.ass', '.vtt', '.sub']
 
 module.exports = {
 	isVideo: file => videoTypes.some(el => file.endsWith(el)),
